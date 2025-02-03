33048cd05c1337fab36d8908c366b67aa0fdc141
kleeding <kawleeding@gmail.com>
2025-02-03 16:26:07 +0000

    added theme toggle

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+var vscale = Math.sqrt(Math.pow(window.innerWidth, 2) + Math.pow(window.innerHeight, 2)) * 2;
+document.documentElement.style.setProperty('--vwh-scale', `${vscale}`);
+
+window.addEventListener("resize", function(event) {
+  vscale = Math.sqrt(Math.pow(window.innerWidth, 2) + Math.pow(window.innerHeight, 2)) * 2
+  document.documentElement.style.setProperty('--vwh-scale', `${vscale}`);
+});
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
